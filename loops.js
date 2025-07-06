e5f8764112edf345d2b1a9f5c2b07e2498a970b9
Bhavy Saraiya <bhavy.14.66@gmail.com>
2025-07-06 11:37:15 +0530

    Commit

---
+++ loops.js
@@ -0,0 +1,32 @@
+console.log("Loops")
+
+let a=1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+//forin
+// let obj ={
+//     name : 'Bhavy',
+//     Address : 'Nadiad'
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element);    
+    
+// }
+
+//forof
+// for (const e of "Bhavy") {
+//     console.log(e);
+// }
+
+//while
+let i=5;
+while(i<6){
+    console.log(i);
+    i++;
+}
